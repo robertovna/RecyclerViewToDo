84db4a2576b2d22331f81f23bc2326cff0d5c319
robertovna <g_arshakyan@list.ru>
2022-05-09 21:37:16 +0300

    correct main fragment with tasks

--- app/src/main/java/com/example/recyclerview/MainFragment.kt
+++ app/src/main/java/com/example/recyclerview/MainFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recyclerview.databinding.ActivityMainBinding
+import com.example.recyclerview.databinding.FragmentMainBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,43 +21,28 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var mainBinding : FragmentMainBinding
+    private lateinit var viewModel: RecycleViewModel
+    private lateinit var taskAdapter: Adapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        mainBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_main, container, false
+        )
+
+        var tasks = IntArray(25) { it }.map { "Item $it" }.toMutableList()
+        taskAdapter = Adapter(tasks)
+        viewModel = RecycleViewModel(taskAdapter)
+        mainBinding.viewModel = viewModel
+        mainBinding.mainRecycler.run {
+            layoutManager = LinearLayoutManager(context)
+            this.adapter = taskAdapter
+        }
+        return mainBinding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
